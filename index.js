135fe41664cb34cd2ab06210199aea90ce8ba345
nwebpro <nwebpro.work@gmail.com>
2022-12-21 18:17:32 +0600

    delete product and delete user

--- index.js
+++ index.js
@@ -73,6 +73,22 @@ app.get('/api/v1/aide/users',  async (req, res) => {
         })
     }
 })
+app.delete('/api/v1/aide/user/:userId', async (req, res) => {
+    try {
+        const userId = req.params.userId
+        const users = await Users.deleteOne({ _id: ObjectId(userId) })
+        res.send({
+            success: true,
+            message: 'User deleted successfully',
+            data: users
+        })
+    } catch (error) {
+        res.send({
+            success: false,
+            error: error.message
+        })
+    }
+})
 
 // All Product Api
 app.post('/api/v1/aide/add/product', async (req, res) => {
@@ -97,6 +113,22 @@ app.get('/api/v1/aide/products',  async (req, res) => {
         res.send({
             success: true,
             message: 'Successfully all products loaded!',
+            data: products
+        })
+    } catch (error) {
+        res.send({
+            success: false,
+            error: error.message
+        })
+    }
+})
+app.delete('/api/v1/aide/product/:productId', async (req, res) => {
+    try {
+        const productId = req.params.productId
+        const products = await Products.deleteOne({ _id: ObjectId(productId) })
+        res.send({
+            success: true,
+            message: 'Product deleted successfully',
             data: products
         })
     } catch (error) {
